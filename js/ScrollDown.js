d8c959ba8c860e216fd2c2eb0bb5393a4c09181b
dowlu <dowlu@hotmail.com>
2017-11-16 18:25:46 +0100

    Add files via upload

--- js/ScrollDown.js
+++ js/ScrollDown.js
@@ -1,4 +1,4 @@
-var gotoMoneySaving = (function(){
+/*var gotoMoneySaving = (function(){
   $('html,body').animate({ scrollTop : $('#MoneySaving').offset().top }, 1000);
 });
 
@@ -16,4 +16,19 @@ var gotoEcoFriendly = (function(){
 
 var gotoSecurity = (function(){
   $('html,body').animate({ scrollTop : $('#Security').offset().top }, 1000);
-});
+});*/
+
+var gotoMoneySaving = (function(){ $('#MoneySaving').show(); });
+var closeMoneySaving = (function(){ $('#MoneySaving').hide(); });
+
+var gotoTravelInfo = (function(){ $('#TravelInfo').show(); });
+var closeTravelInfo = (function(){ $('#TravelInfo').hide(); });
+
+var gotoMeeting = (function(){ $('#Meeting').show(); });
+var closeMeeting = (function(){ $('#Meeting').hide(); });
+
+var gotoEcoFriendly = (function(){ $('#EcoFriendly').show(); });
+var closeEcoFriendly = (function(){ $('#EcoFriendly').hide(); });
+
+var gotoSecurity = (function(){ $('#Security').show(); });
+var closeSecurity = (function(){ $('#Security').hide(); });
--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,17 @@
-var menuOpen = (function(){
-  $('#modalMenu').show();
-})
+var menuOpen = (function(){ $('#modalMenu').show(); })
+var menuClose = (function(){ $('#modalMenu').hide(); })
 
-var menuClose = (function(){
-  $('#modalMenu').hide();
-})
+var gotoMoneySaving = (function(){ $('#MoneySaving').show(); })
+var closeMoneySaving = (function(){ $('#MoneySaving').hide(); })
+
+var gotoTravelInfo = (function(){ $('#TravelInfo').show(); })
+var closeTravelInfo = (function(){ $('#TravelInfo').hide(); })
+
+var gotoMeeting = (function(){ $('#Meeting').show(); })
+var closeMeeting = (function(){ $('#Meeting').hide(); })
+
+var gotoEcoFriendly = (function(){ $('#EcoFriendly').show(); })
+var closeEcoFriendly = (function(){ $('#EcoFriendly').hide(); })
+
+var gotoSecurity = (function(){ $('#Security').show(); })
+var closeSecurity = (function(){ $('#Security').hide(); })
